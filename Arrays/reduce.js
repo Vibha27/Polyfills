c9089afc7b76ff8a1b92e2ffdd5427a1d28e8599
Vibha27 <svibha081@gmail.com>
2024-08-04 14:13:03 +0530

    added arr[0] as initial value in accumulator and starting index with 1 if inital value is not passed

--- Arrays/reduce.js
+++ Arrays/reduce.js
@@ -2,16 +2,14 @@
 // it does not reduce original array
 
 Array.prototype.customReduce = function (callback, initialValue) {
-    let accumulated = initialValue;
-    for(let i=0; i<this.length; i++) {
-        if(accumulated) {
-            accumulated = callback(accumulated, this[i])
-        } else {
-            accumulated = this[i]
-        }
+    let index = 0;
+    let accumulated = initialValue || this[index++];
+    for(let i=index; i<this.length; i++) {
+       accumulated = callback(accumulated, this[i])
     }
     return accumulated
 }
 
 const arr = [1,2,3,4,5]
-console.log(arr.customReduce((acc, curr) => acc+curr))
+console.log(arr.customReduce((acc, curr) => acc+curr, 2))
+console.log(arr.reduce((acc, curr) => acc+curr, 2))
