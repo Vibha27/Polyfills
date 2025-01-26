f052583aaa4b3fc14c901c893d23f4348433865d
Vibha27 <svibha081@gmail.com>
2025-01-26 20:44:21 +0530

    added json stringify support for bigint, symbol and date. corrected object json to convert in array first and then join it by appending {} braces

--- JSON.stringify.js
+++ JSON.stringify.js
@@ -1,22 +1,18 @@
 function stringify(data) {
-    // your code here
-    if (data === undefined) return undefined
-    else if (data === null) return `${data}`
-    else if (typeof data === "function") return undefined
-    else if (typeof data === "number") return `${data}`
-    else if (typeof data === "boolean") return `${data}`
-    else if (typeof data === "string") return `"${data}"`
-    else if (data && Array.isArray(data)) return `[${data}]`
-    else if (typeof data === "object") {
-      let stringObj = Object.keys(data).reduce((acc, curr) => {
-        let obj = `{"${curr}" : ${stringify(data[curr])}}`;
-        acc += obj;
-        return acc
-      }, '')
-      console.log(stringObj, JSON.stringify(data))
-      return stringObj
-    }
-  
+  // your code here
+  if (typeof data === "function") return undefined
+  else if (typeof data === "symbol") return undefined
+  else if (typeof data === "bigint") throw Error("bigint not supported!")
+  else if ([NaN, null, Infinity, undefined].includes(data)) return `null`
+  else if (typeof data === "number") return `${data}`
+  else if (typeof data === "boolean") return `${data}`
+  else if (typeof data === "string") return `"${data}"`
+  else if (data && Array.isArray(data)) return `[${data.map(ele => typeof ele === "symbol" ? "null" : stringify(ele)).join()}]`;
+  else if (data instanceof Date) return `"${data.toISOString()}"`;
+  else if (typeof data === "object") {
+    let stringObj = '{' + Object.keys(data).filter(key => data[key] !== undefined).map(key => `"${key}":${stringify(data[key])}`).join() + '}'
+    return stringObj
   }
-  
-  stringify({a: 1})
+}
+
+stringify({a: { b: [1,2,3,4], c: true, d: undefined} })
